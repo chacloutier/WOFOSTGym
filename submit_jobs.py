17e3865e4c4e05f96881a36a7ac1e4583928213b
chacloutier <northeastcha@gmail.com>
2026-02-26 08:39:33 -0500

    Try with smaller initial lambda

--- submit_jobs.py
+++ submit_jobs.py
@@ -250,6 +250,64 @@ jobs = [
     },
 ]
 
+jobs = [
+    {
+        "name": "PPO_Lag_Rate_Pear",
+        "agent_type": "PPO_Lag_rate",
+        "args": [
+            "--env-id", "perennial-lnpkw-v0",
+            "--agro-file", "pear_agro.yaml",
+            "--env_reward", "RewardScalingWrapper",
+            "--max_n", "80.0",
+            "--max_p", "80.0",
+            "--max_k", "80.0",
+            "--max_w", "40.0",
+            "--npk.max_n", "80.0",
+            "--npk.max_p", "80.0",
+            "--npk.max_k", "80.0",
+            "--npk.max_w", "40.0",
+            "--npk.ag.crop-name", "pear",
+            "--npk.ag.crop-variety", "pear_1",
+            "--npk.intvn_interval", "14",
+            "--track",
+            "--track-resources", "True",
+            "alg:PPO_Lag_rate",
+            "--alg.no-norm-adv",
+            "--alg.num-steps", "4096",
+            "--alg.lagrangian-learning-rate", "0.1",
+            "--alg.initial-lambda", "0.05"
+        ]
+    },
+    {
+        "name": "PPO_Lag_Pear",
+        "agent_type": "PPO_Lag",
+        "args": [
+            "--env-id", "perennial-lnpkw-v0",
+            "--agro-file", "pear_agro.yaml",
+            "--env_reward", "RewardScalingWrapper",
+            "--max_n", "80.0",
+            "--max_p", "80.0",
+            "--max_k", "80.0",
+            "--max_w", "40.0",
+            "--npk.max_n", "80.0",
+            "--npk.max_p", "80.0",
+            "--npk.max_k", "80.0",
+            "--npk.max_w", "40.0",
+            "--npk.ag.crop-name", "pear",
+            "--npk.ag.crop-variety", "pear_1",
+            "--npk.intvn_interval", "14",
+            "--track",
+            "--track-resources", "True",
+            "alg:PPO_Lag",
+            "--alg.no-norm-adv",
+            "--alg.num-steps", "4096",
+            "--alg.cost-limit", "0.1",
+            "--alg.lagrangian-learning-rate", "0.1",
+            "--alg.initial-lambda", "0.05"
+        ]
+    },
+]
+
 # SLURM Template for Compute Canada
 slurm_template = """#!/bin/bash
 #SBATCH --account=def-mcrowley_gpu
